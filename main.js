0d87f315b7dfb8a873e4a598fb601d1c29c88ab9
Ikhsan <ikhsanhumaerah04@gmail.com>
2024-03-04 14:41:09 +0800

    post

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 const cors = require('cors');
-const { Users } = require('./models/users.js');
-const { connect } = require('http2');
 
 const app = express();
 const server = http.createServer(app);
@@ -20,7 +18,10 @@ const connectedUsers = {};
 io.on('connection', (socket) => {
 
     socket.on('login', async (username) => {
+        connectedUsers[socket.id] = username;
         io.emit('userStatus', { username, online: true });
+        const onlineUserCount = Object.keys(connectedUsers).length;
+        io.emit('updateUserCount', onlineUserCount);
     });
 
     socket.on('chat', (data) => {
@@ -34,29 +35,16 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         const username = connectedUsers[socket.id];
         io.emit('userStatus', { username, online: false });
-        updateUserStatusOnDisconnect(username);
+
+        delete connectedUsers[socket.id];
+        const onlineUserCount = Object.keys(connectedUsers).length;
+        io.emit('updateUserCount', onlineUserCount);
     })
 
     socket.on('error', (error) => {
         console.error('Socket.IO Error:', error);
     });
 });
-
-async function updateUserStatusOnDisconnect(username) {
-    try {
-        const response = await fetch('http://localhost:2002/updateUserStatus', {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ username, online: false })
-        });
-
-        const data = await response.json()
-    } catch (error) {
-        console.log(error);
-    }
-}
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
